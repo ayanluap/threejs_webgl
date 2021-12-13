adb8e697b5df54adb02984c8f05c7da7a55739c4
ayanluap <ayanluap@gmail.com>
2021-12-13 17:25:09 +0530

    day02 Transform Obj(position, scale, rotation, group)

---
+++ excercise/03_transform_objects/src/script.js
@@ -0,0 +1,69 @@
+import './style.css'
+import * as THREE from 'three'
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Objects
+ */
+//  create a group
+const group = new THREE.Group()
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+)
+cube2.position.x = -1.5
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x0000ff })
+)
+cube3.position.x = 1.5
+
+group.add(cube3, cube1, cube2)
+group.position.y = 0.5
+group.scale.y = 2
+group.rotation.y = Math.PI / 0.6
+
+// const
+
+// Axes helper
+const axisHelper = new THREE.AxesHelper()
+scene.add(axisHelper)
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: 800,
+  height: 600,
+}
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+// camera.lookAt(mesh.position)
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
